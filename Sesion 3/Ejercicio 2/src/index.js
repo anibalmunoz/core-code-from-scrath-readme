ce0c8b827fdf28b87457f41587bfd8d8a4184394
anibalmunoz <munoz4hernandez@gmail.com>
2022-04-07 15:35:41 -0600

    Sesion 3, Semana 1, Jueves

---
+++ Sesion 3/Ejercicio 2/src/index.js
@@ -0,0 +1,7 @@
+var cond = false;
+
+if (cond==true) {
+  console.log('The cond variable is true');
+} else {
+  console.log('The cond variable is false');
+}
---
+++ Sesion 3/Ejercicio 3/src/index.js
@@ -0,0 +1,9 @@
+var n = 100;
+
+if (n == 100) {
+  console.log('This is a special number!');
+} else if (n < 1000 && n % 10 == 0) {
+  console.log('This number is almost special');
+} else {
+  console.log('Just a regular number');
+}
